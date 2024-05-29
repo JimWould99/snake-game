26aa6a4aaeefc14dad9a2288ab19ce99c1cd456b
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-05-29 16:53:57 +0100

    note: created 1/5/2023

--- file.js
+++ file.js
@@ -3,10 +3,10 @@ var gameStart = false;
 var direction = 1;
 var boxes = document.querySelectorAll(".box");
 var newHead = 1;
-var removed
-var speed = 200
-var lives = document.querySelector('#lives')
-var points = document.querySelector('#points')
+var removed;
+var speed = 200;
+var lives = document.querySelector("#lives");
+var points = document.querySelector("#points");
 var snakeLength = 3;
 var flashInterval;
 let amount = 0;
@@ -15,128 +15,127 @@ let flashCount;
 var startPoints = 0;
 var startLives = 3;
 
-document.addEventListener('click', function () {
-    if (!gameStart){
-        gameStart = true;
-        startSnake();
-    }
-})
+document.addEventListener("click", function () {
+  if (!gameStart) {
+    gameStart = true;
+    startSnake();
+  }
+});
 
-function startSnake(){
-  snakePosition.forEach(element => {
-    boxes[element].classList.add("snake")
-   });
-   randomApple()
-   snakeSpeed()
-  
-};
-
-function snakeSpeed(){
-  speed -= 15
-  intervalId = setInterval(function(){snakeAnimation()}, speed);  
+function startSnake() {
+  snakePosition.forEach((element) => {
+    boxes[element].classList.add("snake");
+  });
+  randomApple();
+  snakeSpeed();
 }
 
-document.addEventListener("keydown", function(event) {
-    if (event.keyCode == 37) {
-      direction = -1;
-    } else if (event.keyCode == 38) {
-      direction = -30;
-    } else if (event.keyCode == 39) {
-      direction = 1;
-    } else if (event.keyCode == 40) {
-      direction = 30
-    }
-})
+function snakeSpeed() {
+  speed -= 15;
+  intervalId = setInterval(function () {
+    snakeAnimation();
+  }, speed);
+}
 
-function snakeAnimation(){
-
-  newHead = snakePosition[0] + direction;
-  
-  if (snakePosition[0] % 30 == 0 && direction == -1) {
-    collusion()
-  } else if (snakePosition[0] % 30 == 0 && direction == 1 && snakePosition[0] != 0) {
-   collusion()
-  } else if (snakePosition[0] <= 29 && direction == -30) {
-   collusion()
-  }  else if(snakePosition[0] >= 570  && direction == 30) {
-   collusion()
-  } else if(boxes[snakePosition[0] + direction].classList.contains('snake')){
-   collusion()
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode == 37) {
+    direction = -1;
+  } else if (event.keyCode == 38) {
+    direction = -30;
+  } else if (event.keyCode == 39) {
+    direction = 1;
+  } else if (event.keyCode == 40) {
+    direction = 30;
   }
-  
-  
+});
 
-  function collusion(){
-    lives.classList.add('hit')
-    setTimeout(function(){
-      lives.classList.remove('hit')
-     }, 500)
+function snakeAnimation() {
+  newHead = snakePosition[0] + direction;
+
+  if (snakePosition[0] % 30 == 0 && direction == -1) {
+    collusion();
+  } else if (
+    snakePosition[0] % 30 == 0 &&
+    direction == 1 &&
+    snakePosition[0] != 0
+  ) {
+    collusion();
+  } else if (snakePosition[0] <= 29 && direction == -30) {
+    collusion();
+  } else if (snakePosition[0] >= 570 && direction == 30) {
+    collusion();
+  } else if (boxes[snakePosition[0] + direction].classList.contains("snake")) {
+    collusion();
+  }
+
+  function collusion() {
+    lives.classList.add("hit");
+    setTimeout(function () {
+      lives.classList.remove("hit");
+    }, 500);
     startLives -= 1;
     if (startLives == 0) {
-      clearInterval(intervalId)
-      alert("Game Over")
+      clearInterval(intervalId);
+      alert("Game Over");
     } else if (startLives == 2) {
-      clearInterval(intervalId)
-      snakeSpeed()
+      clearInterval(intervalId);
+      snakeSpeed();
     } else if (startLives == 1) {
-      clearInterval(intervalId)
-      snakeSpeed()
+      clearInterval(intervalId);
+      snakeSpeed();
     }
-    lives.innerHTML = "Lives: " + startLives
-    snakePosition.forEach(element => {
-      boxes[element].classList.remove('snake')
+    lives.innerHTML = "Lives: " + startLives;
+    snakePosition.forEach((element) => {
+      boxes[element].classList.remove("snake");
     });
-    snakePosition = []
-    newHead = 250
-    console.log(snakePosition)
+    snakePosition = [];
+    newHead = 250;
+    console.log(snakePosition);
     for (let index = 250; index < 250 + snakeLength; index++) {
-      snakePosition.unshift(index)
+      snakePosition.unshift(index);
     }
-    snakePosition.forEach(element => {
-      boxes[element].classList.add('snake')
-    })
-     flashCount = setInterval(flash, 200)
-    console.log(snakePosition) 
+    snakePosition.forEach((element) => {
+      boxes[element].classList.add("snake");
+    });
+    flashCount = setInterval(flash, 200);
+    console.log(snakePosition);
   }
-   
 
   function flash() {
-    amount++
+    amount++;
 
-    snakePosition.forEach(element => {
-      if (boxes[element].classList.contains('snake')) {
-        boxes[element].classList.remove('snake')
+    snakePosition.forEach((element) => {
+      if (boxes[element].classList.contains("snake")) {
+        boxes[element].classList.remove("snake");
       } else {
-        boxes[element].classList.add('snake')
+        boxes[element].classList.add("snake");
       }
-      })
-      }
-     
-      if (amount >= 10) {
-        clearInterval(flashCount)
-      }
-    
+    });
+  }
 
-   snakePosition.unshift(newHead); 
-   removed = snakePosition.pop();
-   boxes[newHead].classList.add("snake")
-   boxes[removed].classList.remove("snake");
+  if (amount >= 10) {
+    clearInterval(flashCount);
+  }
 
-   if(boxes[newHead].classList.contains("apple")){
+  snakePosition.unshift(newHead);
+  removed = snakePosition.pop();
+  boxes[newHead].classList.add("snake");
+  boxes[removed].classList.remove("snake");
+
+  if (boxes[newHead].classList.contains("apple")) {
     snakePosition.push(removed);
     console.log("have apple");
     boxes[newHead].classList.remove("apple");
     randomApple();
     clearInterval(intervalId);
     snakeSpeed();
-    snakeLength++
-    console.log(snakeLength)
+    snakeLength++;
+    console.log(snakeLength);
     startPoints += 1;
-    points.innerHTML = 'Points: ' + startPoints;
-   }
-  
+    points.innerHTML = "Points: " + startPoints;
+  }
 }
-function randomApple(){
+function randomApple() {
   var number = Math.floor(Math.random() * 600);
   boxes[number].classList.add("apple");
 }
